939ab2740829a9c6727f798b8c4180aa505a7b75
HiKami <seimur172@gmail.com>
2021-03-17 08:31:05 +0300

    lab1 finished

--- lab01/lfsr.cpp
+++ lab01/lfsr.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+unsigned get_bit(uint16_t x,
+                 unsigned n) {
+    unsigned bit =  (x >> n) ^ ((x >> (n + 1)) << 1);
+    return bit;
 }
+
+void set_bit(uint16_t * x,
+             unsigned n,
+             unsigned v) {
+     v ? *x |= (1 << n) : *x &= ~(1 << n);
+}
+void lfsr_calculate(uint16_t *reg) {
+    unsigned bit = get_bit(*reg, 0) ^ get_bit(*reg, 2) ^ get_bit(*reg, 3) ^ get_bit(*reg, 5);
+    *reg = *reg >> 1;
+    set_bit(reg, 15, bit);
+}
+
 
